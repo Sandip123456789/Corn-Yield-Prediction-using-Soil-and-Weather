4754754abe9c5399d4ab9319bb3be10c8d27ed8d
Sandip chaudhary <sndp.c114@gmail.com>
2025-12-18 16:04:24 +0100

    Added some updates in XGBoost model(functionality of Search & Report Results) and updated requirements

--- 03_xgboost_tuning.py
+++ 03_xgboost_tuning.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 from sklearn.model_selection import GroupKFold, RandomizedSearchCV
 from sklearn.metrics import r2_score, mean_squared_error
+import joblib
 
 # Configuation
 file_name = 'processed_corn_data.csv'
@@ -35,7 +37,64 @@ param_grid = {
     'learning_rate': [0.01, 0.05, 0.1, 0.2],
 
     # 3. Randomness (Preventing Overfitting)
-    'subsample': []
+    'subsample': [0.6, 0.7, 0.8, 0.9],          # Use only % of rows per tree
+    'colsample_bytree': [0.6, 0.7, 0.8, 0.9],   # Use only % of columns per tree
 
     # 4. Regularization (Penalty for complexity)
+   'reg_alpha': [0, 0.1, 1, 10],             # L1 Regularization
+    'reg_lambda': [0, 1, 10]                 # L2 Regularization
+
 }
+
+# 3. Setup The Search (The "Brain")
+# Must manually generate the GroupKFold splits to pass to the Search
+# This ensures we respect the "Don't split Districts" rule during tuning
+
+gkf = GroupKFold(n_splits=n_folds)
+cv_splits = list(gkf.split(X, y, groups=groups))
+
+xgb_model = xgb.XGBRegressor(
+    objective='reg:squarederror',
+    random_state=42,
+    n_jobs=-1
+)
+
+print(f"   Searching {n_iter} random combinations across {n_folds} folds...")
+print(f"   (This involves fitting {n_iter * n_folds} models. Please wait...)")
+
+search = RandomizedSearchCV(
+    estimator=xgb_model,
+    param_distributions=param_grid,
+    n_iter=n_iter,
+    scoring='neg_root_mean_squared_error',      # Optimize for Lowest RMSE
+    cv=cv_splits,                               # Use our custom Group splits
+    verbose=1,
+    random_state=42,
+    n_jobs=-1
+    # refit=True
+)
+
+# Run the Search
+search.fit(X, y)
+
+#4. Report Results
+best_model = search.best_estimator_
+best_params = search.best_params_
+best_rmse = -search.best_score_ # Flip sign back to positive RMSE
+
+print("\n" + "="*40)
+print(f"Champion Model Found.")
+print("="*40)
+print(f"Best RMSE (Validation): {best_rmse:.4f}")
+print("Best Parameters:")
+for param, value in best_params.items():
+    print(f" - {param}: {value}")
+
+# # Saving the Model
+# best_model.save_model('best_corn_xgboost.json')
+# print("\n Saved best model to 'best_corn_xgboost.json'")
+
+# Saving the Model using joblib
+model_filename = "best_corn_xgboost.pkl"
+joblib.dump(best_model, model_filename)
+print(f"\n Saved best model to '{model_filename}' (using joblib)")
